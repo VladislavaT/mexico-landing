15874dfb301891dfd050c796ca49aaff520ab9bb
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-09-01 19:03:41 +0300

    added script for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,3 @@ function submitForm(event) {
 }
 
 form.addEventListener('submit', submitForm);
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page--with-menu');
-  } else {
-    document.body.classList.remove('page--with-menu');
-  }
-});
